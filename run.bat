ab2685ae787f6430e7625dd4aba393f825ba4b5a
ahmedkhaled202p-coder <ahmedkhaled202p@gmail.com>
2026-07-15 12:52:31 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if "%ORBIT_HR_HOST%"=="" set ORBIT_HR_HOST=127.0.0.1
-if "%ORBIT_HR_PORT%"=="" set ORBIT_HR_PORT=8080
-REM Set ORBIT_HR_PUBLIC_ORIGIN and ORBIT_HR_ALLOWED_ORIGINS in .env after choosing your domain.
-python start_server.py
